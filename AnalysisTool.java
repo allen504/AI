bd56d50d789cb1462fef89ca83cfec13ce62f09f
GAllen632 <60718224+GAllen632@users.noreply.github.com>
2020-03-08 08:46:22 -0500

    Update AnalysisTool.java

--- AnalysisTool.java
+++ AnalysisTool.java
@@ -8,24 +8,26 @@ public class AnalysisTool {
 
     public static double getTeslaStockPrice() throws IOException{
         String a = "";
-        //Make a doc to retrieve an element from
+        //Make a document to retrieve an element from a website
         Document doc = Jsoup.connect("https://finance.yahoo.com/quote/TSLA/").get();
-        // retrieve an element
+        // retrieve the element
         Elements title = doc.select("span[class=Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)]");
         // get string value from the element
         a = title.text();
+        // parse the string value into a double and return it
         double b = Double.parseDouble(a);
         return b;
     }
 
     public static double getTeslaOpenPrice() throws IOException{
         String c = "";
-        //Make a doc to retrieve an element from
+        //Make a document to retrieve an element from a website
         Document doc = Jsoup.connect("https://finance.yahoo.com/quote/TSLA/").get();
-        // retrieve an element
+        // retrieve the element
         Elements title = doc.select("td[class=Ta(end) Fw(600) Lh(14px)].span[class=Trsdu(0.3s)]");
         // get string value from the element
         c = title.text();
+        // parse the string value into a double and return it
         double d = Double.parseDouble(c);
         return d;
     }
